65a76c65318f670ad452e41ec21170d635a2bffc
Anmol <anmolsaxena456@gmail.com>
2024-02-15 00:39:05 -0600

    need for styling framework
    have to regenerate application using standalone in order to utilize angular material style framework

---
+++ src/app/algorithms.ts
@@ -0,0 +1,50 @@
+export const sorts: any[] = [
+	{
+		'name': 'Bubble Sort',
+		'best-case': 'O(n)',
+		'worst-case': 'O(n^2)',
+		'average': 'O(n^2)',
+	},
+	{
+		'name': 'Selection Sort',
+		'best-case': 'O(n^2)',
+		'worst-case': 'O(n^2)',
+		'average': 'O(n^2)',
+	},
+	{
+		'name': 'Insertion Sort',
+		'best-case': 'O(n)',
+		'worst-case': 'O(n^2)',
+		'average': 'O(n^2)',
+	},
+	{
+		'name': 'Merge Sort',
+		'best-case': 'O(n log n)',
+		'worst-case': 'O(n log n)',
+		'average': 'O(n log n)',
+	},
+	{
+		'name': 'Quick Sort',
+		'best-case': 'O(n log n)',
+		'worst-case': 'O(n^2)',
+		'average': 'O(n log n)',
+	},
+	{
+		'name': 'Heap Sort',
+		'best-case': 'O(n log n)',
+		'worst-case': 'O(n log n)',
+		'average': 'O(n log n)',
+	},
+	{
+		'name': 'Radix Sort',
+		'best-case': 'O(nk)',
+		'worst-case': 'O(nk)',
+		'average': 'O(nk)',
+	},
+	{
+		'name': 'Counting Sort',
+		'best-case': 'O(n + k)',
+		'worst-case': 'O(n + k)',
+		'average': 'O(n + k)',
+	},
+];
