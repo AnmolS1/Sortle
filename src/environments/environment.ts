04d6450aae4fac88b4dfaaf4fd072393f86c3956
Anmol <anmolsaxena456@gmail.com>
2024-03-05 12:08:13 -0600

    *sigh*

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-	production: false,
-	AWS_REGION: 'us-east-2',
-	AWS_ACCESS_KEY_ID: 'AKIASFEPHB54UG2SRIPK',
-	AWS_SECRET_ACCESS_KEY: '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'
-};
