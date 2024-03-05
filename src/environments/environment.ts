26eae1ecec16a681bb7742f075eb79f2946adba9
Anmol <anmolsaxena456@gmail.com>
2024-03-05 12:06:56 -0600

    pushing for a vercel deployment, will test vercel serverless functions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: false,
 	AWS_REGION: 'us-east-2',
-	AWS_ACCESS_KEY_ID: 'AKIASFEPHB543ST4S2OB',
-	AWS_SECRET_ACCESS_KEY: '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'
+	AWS_ACCESS_KEY_ID: 'AKIASFEPHB54UG2SRIPK',
+	AWS_SECRET_ACCESS_KEY: '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'
 };
